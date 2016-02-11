6a95e5460aaebe9c0a5b00d3183b22aefaa705c3
Stephano <amunra@unm.edu>
2016-02-11 16:24:46 -0700

    Added the initial source folder with a default package structure (cs351.project1). It has a Project1.java file in it, but that was just so Github would let me make the folders since it tends to delete empty folders.

---
+++ src/cs351/project1/Project1.java
@@ -0,0 +1,7 @@
+package cs351.project1;
+
+// Created this since Github gets mad if you have empty folders.
+// Once we start adding project files here we can delete this class.
+public class Project1
+{
+}
