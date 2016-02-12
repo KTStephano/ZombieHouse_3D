73ad74fbad41d22e64beed1b1e1a09f059cc1afc
Stephano <amunra@unm.edu>
2016-02-12 15:46:46 -0700

    Added a useful println.

--- src/cs351/project1/Project1.java
+++ src/cs351/project1/Project1.java
@@ -4,6 +4,11 @@ package cs351.project1;
 // Once we start adding project files here we can delete this class.
 public class Project1
 {
+  public void testMerge()
+  {
+    System.out.println("Please work");
+  }
+
  /*
   * test of git
   */
