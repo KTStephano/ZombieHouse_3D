5fe85b0c998a6b5f56e5495b743a15061ae2cfda
Scott Cooper <scott@websitesabq.com>
2016-02-13 14:45:27 -0700

    Sound Engine Updates
    Created SoundStackItem
    Updated queueSoundAtLocation in SoundEngine

---
+++ src/cs351/project1/SoundStackItem.java
@@ -0,0 +1,15 @@
+package cs351.project1;
+
+import javafx.scene.media.Media;
+
+public class SoundStackItem {
+  Media sound;
+  int x;
+  int y;
+  public SoundStackItem(Media sound, int x, int y)
+  {
+    this.sound = sound;
+    this.x = x;
+    this.y = y;
+  }
+}
--- src/cs351/project1/ZombieHouseSoundEngine.java
+++ src/cs351/project1/ZombieHouseSoundEngine.java
@@ -1,10 +1,15 @@
 package cs351.project1;
 
 import cs351.core.SoundEngine;
+import java.util.*;
+
 import javafx.scene.media.Media;
 import java.awt.Point;
+
 public class ZombieHouseSoundEngine implements SoundEngine {
   private Point centralPoint;
+  
+  private Stack<SoundStackItem> soundStack = new Stack<>();
   @Override
   public void setCentralPoint(int x, int y) 
   {
@@ -15,14 +20,16 @@ public class ZombieHouseSoundEngine implements SoundEngine {
   @Override
   public void queueSoundAtLocation(Media sound, int x, int y) 
   {
-    // TODO establish priority queue containing
-    // sound and distance to centralPoint = sqrt((cp.x - x)^2 + (cp.y-y)^2)
-    
+    soundStack.push(new SoundStackItem(sound,x,y));   
   }
 
   @Override
   public void update() {
     // TODO Auto-generated method stub
+
+    // NOTE: x, y distance to centralPoint = sqrt((cp.x - x)^2 + (cp.y-y)^2)
+    // determines volumne during playback
+    
     
   }
 
