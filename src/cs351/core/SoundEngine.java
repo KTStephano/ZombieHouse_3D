be0adc1f1578db735535cb8519e04c7366277c3a
Stephano <amunra@unm.edu>
2016-02-12 19:46:59 -0700

    Added the SoundEngine interface so that the class can be implemented by Scott.

---
+++ src/cs351/core/SoundEngine.java
@@ -0,0 +1,45 @@
+package cs351.core;
+
+import javafx.scene.media.Media;
+
+/**
+ * The idea behind a SoundEngine is that each frame a number of objects
+ * can request that a sound be played relative to a central point (ex: a player location).
+ *
+ * Sounds that are closer to the specified central point should play at
+ * the maximum default volume, while sounds further away from the central
+ * point should decrease in volume in some way.
+ */
+public interface SoundEngine
+{
+  /**
+   * When this function is called the old central point should be overridden
+   * and all future requests to play sounds should use the new point
+   * in their volume calculations.
+   *
+   * @param x center x-coordinate
+   * @param y center y-coordinate
+   */
+  void setCentralPoint(int x, int y);
+
+  /**
+   * This function should not immediately play the sound at location
+   * (x, y) but should instead add it to a queue. When the SoundEngine is told
+   * to flush all sounds from its queue, that is the point where all sounds
+   * should be played.
+   *
+   * The reason for this delay is so that the SoundEngine can blend/merge/etc. the
+   * sounds it gets so that they sound better to the user.
+   *
+   * @param sound Media object to play
+   * @param x x-coordinate where the sound started
+   * @param y y-coordinate where the sound started
+   */
+  void queueSoundAtLocation(Media sound, int x, int y);
+
+  /**
+   * When this function is called, all sounds that were added to the sound queue
+   * through the queueSoundAtLocation function should be played appropriately.
+   */
+  void update();
+}
