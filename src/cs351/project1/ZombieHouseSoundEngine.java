8b2d6d4097a2209dd90fdd126d17c37debd8f6d3
Scott Cooper <scott@websitesabq.com>
2016-02-13 15:21:18 -0700

    Updated Sound Engine
    Sound Engine update function now plays sounds until sound queue empty.
    Basic implementation.  Test to see what works

--- src/cs351/project1/ZombieHouseSoundEngine.java
+++ src/cs351/project1/ZombieHouseSoundEngine.java
@@ -4,12 +4,14 @@ import cs351.core.SoundEngine;
 import java.util.*;
 
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import java.awt.Point;
 
 public class ZombieHouseSoundEngine implements SoundEngine {
-  private Point centralPoint;
   
+  private Point centralPoint;
   private Stack<SoundStackItem> soundStack = new Stack<>();
+  
   @Override
   public void setCentralPoint(int x, int y) 
   {
@@ -25,10 +27,25 @@ public class ZombieHouseSoundEngine implements SoundEngine {
 
   @Override
   public void update() {
+    SoundStackItem tmpSoundStackItem;
+    MediaPlayer soundPlayer;
     // TODO Auto-generated method stub
 
     // NOTE: x, y distance to centralPoint = sqrt((cp.x - x)^2 + (cp.y-y)^2)
     // determines volume during playback
+    
+    // Below is a coarse single threaded 
+    // implementation of a sound engine 
+    // media player.
+    
+    //  This was created as a test to see how
+    //  well a very basic approach would work.
+    while (soundStack.isEmpty()) 
+    {
+      tmpSoundStackItem = soundStack.pop();
+      soundPlayer = new MediaPlayer(tmpSoundStackItem.sound);
+      soundPlayer.play(); 
+     }
     
     
   }
